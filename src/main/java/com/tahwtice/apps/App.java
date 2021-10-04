4bd1fe4d3ec249121605ec4c19008b7babda3b39
tahwtice <fengjunasd@126.com>
2021-10-04 10:48:36 +0800

    refactor with Excel service controller

--- src/main/java/com/tahwtice/apps/App.java
+++ src/main/java/com/tahwtice/apps/App.java
@@ -2,22 +2,14 @@ package com.tahwtice.apps;
 
 // https://techblogstation.com/java/read-and-write-excel-file-in-java/
 
-import java.util.List;
-
-import com.tahwtice.apps.model.Billing;
-import com.tahwtice.apps.model.Order;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        ExcelParser parser = new ExcelParser();
-        List<Order> orderList = parser.parseOrder();
-        orderList.forEach(System.out::println);
-        List<Billing> billingList = parser.parseBilling();
-        billingList.forEach(System.out::println);
-
-        ExcelReporter reporter = new ExcelReporter();
-        reporter.export(billingList);
+        ExcelService service = new ExcelService();
+        service.parse();
+        service.vLookUp();
+        service.export();
     }
 }
---
+++ src/main/java/com/tahwtice/apps/ExcelService.java
@@ -0,0 +1,40 @@
+package com.tahwtice.apps;
+
+import java.util.List;
+
+import com.tahwtice.apps.model.Billing;
+import com.tahwtice.apps.model.Order;
+
+public class ExcelService {
+    private final ExcelParser parser;
+    private final ExcelReporter reporter;
+    private List<Order> orderList;
+    private List<Billing> billingList;
+    private List<Billing> finalBillingList;
+
+    public ExcelService() {
+        this.parser = new ExcelParser();
+        this.reporter = new ExcelReporter();
+
+        // this.orderList = new ArrayList<>();
+        // this.billingList = new ArrayList<>();
+        // this.finalBillingList = new ArrayList<>();
+    }
+
+    public final void parse() {
+        this.orderList = this.parser.parseOrder();
+        this.billingList = this.parser.parseBilling();
+
+        this.orderList.forEach(System.out::println);
+        this.billingList.forEach(System.out::println);
+    }
+
+    public final void vLookUp() {
+        this.finalBillingList = this.billingList;
+    }
+
+    public final void export() {
+        this.finalBillingList.forEach(System.out::println);
+        this.reporter.export(this.finalBillingList);
+    }
+}
