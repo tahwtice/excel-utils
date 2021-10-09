149ab4c78a572763d62fe5628d429b47351fd040
tahwtice <fengjunasd@126.com>
2021-10-09 14:28:33 +0800

    refactor

--- src/main/java/com/tahwtice/apps/model/Billing.java
+++ src/main/java/com/tahwtice/apps/model/Billing.java
@@ -6,22 +6,11 @@ import lombok.Data;
 public class Billing {
     private String guid;
 
-    private String billing;
     private String salesOrder;
-    private String customerPO;
-    private String payer;
-    private String payerName;
     private String materialCode;
-    private String materialDescription;
     private double unitPrice;
     private double quantity;
     private double totalValue;
-    private double item;
-
-    private String finalBilling;
-    private double finalQuantity;
-    private double finalTotalValue;
-    private String finalPO;
 
     private int rowIndex;
     private boolean deleted = true;
--- src/main/java/com/tahwtice/apps/model/Order.java
+++ src/main/java/com/tahwtice/apps/model/Order.java
@@ -1,25 +1,12 @@
 package com.tahwtice.apps.model;
 
-import java.time.LocalDate;
-
 import lombok.Data;
 
 @Data
 public class Order {
     private String guid;
 
-    private double systemId;
-    private LocalDate orderDate;
-    private String group;
-    private String person;
-    private String phone;
-    private String cellphone;
-    private String mail;
-    private String brand;
     private String material;
-    private String packageSize;
-    private String name;
-    private String level;
     private double unitPrice;
     private double quantity;
     private double totalPrice;
